47d999c1003db7a884e42568c843afaa424be3c7
jayanthkumarvydana <jayanthkumarvydana@gmail.com>
2025-10-31 22:23:36 +0530

    Implemented Splash Screen

---
+++ app/src/main/java/jayanthkumar/project/realestateapp/LoginActivity.kt
@@ -0,0 +1,17 @@
+package jayanthkumar.project.realestateapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
